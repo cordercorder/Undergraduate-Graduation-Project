348d800a0b089e2d343326d8f489c3097e3a0937
cordercorder <2205722269@qq.com>
2020-05-22 19:53:54 +0800

    add combine all features

--- classify/run_combine_feature_logistic_classify.sh
+++ classify/run_combine_feature_logistic_classify.sh
@@ -1,7 +1,9 @@
 python -u combine_feature_logistic_classify.py \
     --feature_name syntax_wals \
-    --source_dir /data/rrjin/Graduation/data/language_vector \
+    --source_dir /data/rrjin/Graduation/data/cell_states \
+                /data/rrjin/Graduation/data/language_vector \
                 /data/rrjin/Graduation/data/hidden_states \
-    --prefix combine_all_data_bpe_ \
+    --prefix cell_states_ \
+             combine_all_data_bpe_ \
              hidden_states_ \
-    --output_file_name combine_word_vec_hidden_states_logistic_classify_syntax_wals.txt
+    --output_file_name combine_cell_states_word_vec_hidden_states_logistic_classify_syntax_wals.txt
